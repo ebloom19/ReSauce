47a51d71a563116c19272fcece851546738a7af7
Mohamad <m.antar1996@hotmail.com>
2021-10-28 19:05:19 +1100

    heroku deployment commands

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = require("./database/db"); // The postgres connection we already have
+const expressSession = require("express-session"); // Express library to handle sessions
+
+
+// We need to store sessions in the DB, otherwise it'll forget them all when you restart the server.
+const connectPgSimple = require("connect-pg-simple");
+const pgSession = connectPgSimple(expressSession);
+
+const recipeRouter = require('./controllers/recipesRouter');
+const usersRouter = require('./controllers/users');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(
+    expressSession({
+      store: new pgSession({
+        pool: db, // Connects to our postgres db
+        createTableIfMissing: true, // Creates a session table in your database (go look at it!)
+      }),
+      secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+    })
+  );
+
+
+app.use(express.json());
+app.use(express.static("client"));
+
+app.use('/recipes', recipeRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`listening on port  http://localhost:${port}`);
+});
---
+++ database/db.js
@@ -0,0 +1,19 @@
+const pg = require("pg");
+
+let db;
+if (process.env.NODE_ENV === 'production') {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  db = new pg.Pool({
+    database: "meal_reboot",
+    password: 'antar525' // If you have a password on your local db
+  })
+}
+
+
+module.exports = db;
