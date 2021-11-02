73d90fca329d12963c1ad9caffe576336595122e
dan murphy <djnmurphy@hotmail.co.uk>
2021-11-02 18:40:11 +1100

    loggin message fix

--- client/js_components/initialize.js
+++ client/js_components/initialize.js
@@ -2,8 +2,8 @@ console.log("hello world")
 
 header()
 
-headerNav = document.getElementById("header-nav")
-loggedIn = document.createElement('h4')
+const headerNav = document.getElementById("header-nav")
+const loggedIn = document.createElement('h4')
 headerNav.append(loggedIn)
 
 const mainDiv = document.createElement("div")
@@ -13,19 +13,22 @@ document.body.append(mainDiv)
 // checks if someone is logged in and displays there name 
 function loggInMessage() {
     axios.get('/users').then((res) => {
-        loggedIn.innerHTML = "Logged In As:  " + res.data.message
+        loggedIn.innerText = "Logged In As:  " + res.data.message
+
+        ingredients()
+        
+        
      })
 
 }
-
+home()
 loggInMessage()
 
+console.log(loggedIn.innerText)
+
+
 // home if not logged in and ingredients page if and when logged in
-if (loggedIn.innerHTML === "") {
-    home()
-}else{
-    ingredients()
-}
+
 
 
 
