c114918a5f22ae77a6fa6d107bb0e7b2de9e3d33
Ethan Bloom <ebloom19@gmail.com>
2022-01-27 13:42:01 +0800

    pre pull changes

--- models/favRecipes_model.js
+++ models/favRecipes_model.js
@@ -3,20 +3,20 @@ const db = require("../database/db");
 const favRecipes = {
     addRecipe(id, favRecipe, title, img) {
         return db
-    .query("INSERT INTO favRecipes (users_id, recipe_id, title, img) VALUES ($1, $2)", [id, favRecipe, title, img])
-    .then((dbRes) => dbRes.rows);
+            .query("INSERT INTO favRecipes (users_id, recipe_id, title, img) VALUES ($1, $2)", [id, favRecipe, title, img])
+            .then((dbRes) => dbRes.rows);
     },
 
     getRecipe(id) {
         return db
-    .query("SELECT * FROM favRecipes")
-    .then((dbRes) => dbRes.rows);
+            .query("SELECT * FROM favRecipes")
+            .then((dbRes) => dbRes.rows);
     },
 
     deleteIngredients(id) {
         return db
-    .query("DELETE FROM favRecipes WHERE id = $1", [id])
-    .then((dbRes) => dbRes.rows);
+            .query("DELETE FROM favRecipes WHERE id = $1", [id])
+            .then((dbRes) => dbRes.rows);
     }
 
 
